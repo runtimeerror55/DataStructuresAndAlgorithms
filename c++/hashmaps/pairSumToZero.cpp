094fb3489771e0898287f816bd7413f2da633bd9
aakash deep <runtimeerror55@gmail.com>
2022-03-22 23:37:54 +0530

    added to c++

--- c++/hashmaps/pairSumToZero.cpp
+++ c++/hashmaps/pairSumToZero.cpp
@@ -3,48 +3,19 @@ using namespace std;
 
 int pairSum(int input[], int size)
 {
-    unordered_map<int, int> um;
+
+    unordered_map<int, int> frequency;
     int count = 0;
-    bool flag = true;
     for (int i = 0; i < size; i++)
     {
-        um[input[i]]++;
-        if (i != size - 1 && input[i] != input[i + 1])
+        int compliment = -input[i];
+        if (frequency.find(compliment) != frequency.end())
         {
-            flag = false;
+            count += frequency[compliment];
         }
+        frequency[input[i]]++;
     }
 
-    if (flag)
-    {
-        if (input[0] == 0)
-        {
-            return ((size - 1) * (size)) / 2;
-        }
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        int elementOneCount = um[input[i]];
-
-        if (elementOneCount > 0)
-        {
-            int elementTwoCount = um[-input[i]];
-            if (elementTwoCount > 0)
-            {
-                if (input[i] == 0)
-                {
-                    count += (elementOneCount - 1) * (elementOneCount) / 2;
-                }
-                else
-                {
-                    count += elementOneCount * elementTwoCount;
-                }
-                um[input[i]];
-                um[-input[i]] = 0;
-            }
-        }
-    }
     return count;
 }
 int main()
